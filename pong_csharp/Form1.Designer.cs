61332bfe0a115a395f81fd87aa35ef271030557d
SidewinderJoe <snidejere@gmail.com>
2018-08-19 23:21:18 -0400

    first commit

---
+++ pong_csharp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace pong_csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.cpu = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerScore = new System.Windows.Forms.Label();
+            this.cpuLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.AccessibleName = "";
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(12, 186);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(27, 127);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ball
+            // 
+            this.ball.AccessibleName = "";
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(434, 239);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(27, 26);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // cpu
+            // 
+            this.cpu.BackColor = System.Drawing.Color.White;
+            this.cpu.Location = new System.Drawing.Point(897, 230);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(27, 127);
+            this.cpu.TabIndex = 2;
+            this.cpu.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 5;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.BackColor = System.Drawing.Color.Black;
+            this.playerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScore.ForeColor = System.Drawing.Color.White;
+            this.playerScore.Location = new System.Drawing.Point(105, 9);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(38, 25);
+            this.playerScore.TabIndex = 3;
+            this.playerScore.Text = "00";
+            // 
+            // cpuLabel
+            // 
+            this.cpuLabel.AutoSize = true;
+            this.cpuLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.cpuLabel.Location = new System.Drawing.Point(735, 9);
+            this.cpuLabel.Name = "cpuLabel";
+            this.cpuLabel.Size = new System.Drawing.Size(38, 25);
+            this.cpuLabel.TabIndex = 4;
+            this.cpuLabel.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(928, 574);
+            this.Controls.Add(this.cpuLabel);
+            this.Controls.Add(this.playerScore);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Pong Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label cpuLabel;
+    }
+}
+
---
+++ pong_csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pong_csharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pong_csharp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("579a0d94-1751-4657-be5f-fec7ff2e092c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
